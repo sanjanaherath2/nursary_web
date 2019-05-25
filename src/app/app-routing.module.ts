20c49a451c98856d4d6e3d22c2df6ca0abc14fd1
sanjanaherath2 <sanjanaherath2@gmail.com>
2019-05-25 14:23:22 +0530

    all files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from './components/landing/landing.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent},]
+
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    
   ],
   imports: [
     BrowserModule,
