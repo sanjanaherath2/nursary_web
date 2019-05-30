a7eaf0ccfa4bed6ac488419ac994da003e5c2429
sanjanaherath2 <sanjanaherath2@gmail.com>
2019-05-30 11:22:02 +0530

    new routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import {LandingComponent} from './components/landing/landing.component';
 import {AdminDashboardComponent} from './components/admin/admin-dashboard/admin-dashboard.component';
 import {UserProfileComponent} from './components/user/user-profile/user-profile.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
@@ -11,7 +13,8 @@ const routes: Routes = [
    
 
  // Auth routes
-
+ {path: 'register', component: RegisterComponent},
+ {path:'sign-in', component: SignInComponent},
 
  // admin routes
  { path: 'admin-dashboard', component: AdminDashboardComponent},
