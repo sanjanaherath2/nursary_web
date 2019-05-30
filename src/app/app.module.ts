e54b8fb66a9fe0982d3b2630a8e5fc66563c0b2f
sanjanaherath2 <sanjanaherath2@gmail.com>
2019-05-30 11:11:01 +0530

    authentication added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import {AdminDashboardComponent} from './components/admin/admin-dashboard/admin-dashboard.component';
 import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 
 
@@ -18,7 +21,10 @@ import { UserProfileComponent } from './components/user/user-profile/user-profil
     NavbarComponent,
     WelcomeComponent,
     AdminDashboardComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    AuthComponent,
+    SignInComponent,
+    RegisterComponent
    
     
   ],
